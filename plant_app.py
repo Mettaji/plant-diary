f77c3c0843368a36593dca700303e6b55181a8b6
Mettaji <107264851+Mettaji@users.noreply.github.com>
2026-04-08 14:33:20 +1000

    Update plant_app.py

--- plant_app.py
+++ plant_app.py
@@ -155,7 +155,6 @@ elif choice == "Add New Plant":
                 st.image(st.session_state['last_upload'], width=300)
 
 
-st.write(f"Available keys: {list(st.secrets.keys())}")
 
 
 # ------------------------------------------
